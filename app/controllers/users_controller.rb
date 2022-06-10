5d4a1401e2417d22d71b616412f0a3946a81ff53
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-06-10 12:06:30 -0700

    updated patch controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    @current_user.image.purge
+    # @current_user.image.purge
     @current_user.update!(user_params)
     render json: @current_user
   end
