861c34b7cef438c04b6cd37d1c18f614c790379f
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-06-10 21:00:51 -0700

    added tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Tag.create( name: "Ride")
+Tag.create( name: "Run")
+Tag.create( name: "Swim")
+Tag.create( name: "Hike")
+Tag.create( name: "Walk")
+Tag.create( name: "Alpine Ski")
+Tag.create( name: "Backcountry Ski")
+Tag.create( name: "Canoe")
+Tag.create( name: "Crossfit")
+Tag.create( name: "Elliptical")
+Tag.create( name: "Inline Skate")
+Tag.create( name: "Kayaking")
+Tag.create( name: "Rowing")
+Tag.create( name: "Snowboard")
+Tag.create( name: "Surfing")
+Tag.create( name: "Weight Training")
+Tag.create( name: "Yoga")
